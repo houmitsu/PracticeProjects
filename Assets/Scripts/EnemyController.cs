7af3fbd8d7f12934de687808586316e70e5af70e
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-02-26 20:21:59 +0900

    Add Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("プレイヤーを検出した　-　攻撃せよ");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("プレイヤーは領域外。パトロール続行");
+        }
+    }
+}
